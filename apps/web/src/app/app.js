fc80990959e5e651baa6aac8e8ddcbe22fe7dcb9
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2025-09-26 08:24:03 +0100

    feat: clickable links

--- apps/web/src/app/app.js
+++ apps/web/src/app/app.js
@@ -10,5 +10,25 @@ import { createBaseApp } from '@pins/casework-portal-lib/app/app.js';
 export function createApp(service) {
 	const router = buildRouter(service);
 	// create an express app, and configure it for our usage
-	return createBaseApp({ service, configureNunjucks, router, middlewares: [addLocalsConfiguration()] });
+	return createBaseApp({
+		service,
+		configureNunjucks,
+		router,
+		middlewares: [addLocalsConfiguration()],
+		cspDirectives
+	});
 }
+
+/** @type {import('helmet').ContentSecurityPolicyOptions['directives']} */
+const cspDirectives = {
+	scriptSrc: [
+		"'self'",
+		"'sha256-EkKo+C5UwqgMN26GNVh01ET+DM5zmVk0irXY7ci7frA='",
+		(req, res) => `'nonce-${res.locals.cspNonce}'`
+	],
+	defaultSrc: ["'self'"],
+	connectSrc: ["'self'"],
+	fontSrc: ["'self'"],
+	imgSrc: ["'self'"],
+	styleSrc: ["'self'"]
+};
