56edd486bd40919978dd1f599e173b25f1ca1b59
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2026-02-10 16:50:25 +0000

    chore(web): support govuk-frontend v6

--- apps/web/src/util/config-middleware.js
+++ apps/web/src/util/config-middleware.js
@@ -6,7 +6,7 @@ export function addLocalsConfiguration() {
 	return (req, res, next) => {
 		res.locals.config = {
 			headerTitle: 'Manage casework',
-			styleFile: 'style-15dfedb7.css'
+			styleFile: 'style-695eb0a1.css'
 		};
 		next();
 	};
