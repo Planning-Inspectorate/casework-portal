c9d162c5056d1aa09aacb1a0944ebd634b8b7110
Michael Juckes <michael.juckes@planninginspectorate.gov.uk>
2026-03-02 14:03:29 +0000

    feat: adding private endpoints for key vualts

--- infrastructure/data.tf
+++ infrastructure/data.tf
@@ -40,3 +40,10 @@ data "azurerm_private_dns_zone" "app_service" {
 
   provider = azurerm.tooling
 }
+
+data "azurerm_private_dns_zone" "keyvault" {
+  name                = "privatelink.vaultcore.azure.net"
+  resource_group_name = var.tooling_config.network_rg
+
+  provider = azurerm.tooling
+}
--- infrastructure/key-vault.tf
+++ infrastructure/key-vault.tf
@@ -1,16 +1,19 @@
 resource "azurerm_key_vault" "main" {
-  #checkov:skip=CKV_AZURE_109: TODO: consider firewall settings, route traffic via VNet
-  #checkov:skip=CKV_AZURE_189: "Ensure that Azure Key Vault disables public network access"
-  #checkov:skip=CKV2_AZURE_32: "Ensure private endpoint is configured to key vault"
-  name                        = "${local.org}-kv-${local.short_resource_suffix}"
-  location                    = module.primary_region.location
-  resource_group_name         = azurerm_resource_group.primary.name
-  enabled_for_disk_encryption = true
-  tenant_id                   = data.azurerm_client_config.current.tenant_id
-  soft_delete_retention_days  = 7
-  purge_protection_enabled    = true
-  rbac_authorization_enabled  = true
-  sku_name                    = "standard"
+  name                          = "${local.org}-kv-${local.short_resource_suffix}"
+  location                      = module.primary_region.location
+  resource_group_name           = azurerm_resource_group.primary.name
+  enabled_for_disk_encryption   = true
+  tenant_id                     = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days    = 7
+  purge_protection_enabled      = true
+  rbac_authorization_enabled    = true
+  public_network_access_enabled = false
+  sku_name                      = "standard"
+
+  network_acls {
+    bypass         = "AzureServices"
+    default_action = "Deny"
+  }
 
   tags = local.tags
 }
@@ -25,13 +28,14 @@ resource "azurerm_key_vault_secret" "manual_secrets" {
   value        = "<terraform_placeholder>"
   content_type = "plaintext"
 
-  tags = local.tags
-
+  depends_on = [azurerm_private_dns_zone_virtual_network_link.keyvault]
   lifecycle {
     ignore_changes = [
       value
     ]
   }
+
+  tags = local.tags
 }
 
 locals {
@@ -49,4 +53,23 @@ locals {
       "session-secret"                 = "@Microsoft.KeyVault(SecretUri=${azurerm_key_vault_secret.session_secret.versionless_id})"
     }
   )
+}
+
+resource "azurerm_private_endpoint" "keyvault" {
+  name                = "${local.org}-pe-keyvault-${local.resource_suffix}"
+  location            = module.primary_region.location
+  resource_group_name = azurerm_resource_group.primary.name
+  subnet_id           = azurerm_subnet.main.id
+
+  private_dns_zone_group {
+    name                 = "${local.org}-pdns-${local.service_name}-keyvault-${var.environment}"
+    private_dns_zone_ids = [data.azurerm_private_dns_zone.keyvault.id]
+  }
+
+  private_service_connection {
+    name                           = "${local.org}-psc-keyvault-${local.resource_suffix}"
+    private_connection_resource_id = azurerm_key_vault.main.id
+    subresource_names              = ["vault"]
+    is_manual_connection           = false
+  }
 }
