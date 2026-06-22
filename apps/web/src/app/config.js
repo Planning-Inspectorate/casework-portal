2e67e66912d55ae0cf2a15dbaafbef16bd4934a0
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2026-06-22 15:42:55 +0100

    chore(deps): replace dotenv with process.loadEnvFile (no-ticket)

--- apps/web/src/app/config.js
+++ apps/web/src/app/config.js
@@ -1,4 +1,4 @@
-import dotenv from 'dotenv';
+import { loadEnvFile } from 'node:process';
 import path from 'node:path';
 import { fileURLToPath } from 'url';
 
@@ -26,7 +26,11 @@ export function loadConfig() {
 		return config;
 	}
 	// load configuration from .env file into process.env
-	dotenv.config();
+	try {
+		loadEnvFile();
+	} catch {
+		/* ignore errors*/
+	}
 
 	// get values from the environment
 	const {
