bb0971393ed931eb28c252910ccdeb4d941b1fb7
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2026-06-25 11:31:15 +0100

    style: adds new pins footer

--- apps/web/src/util/config-middleware.js
+++ apps/web/src/util/config-middleware.js
@@ -6,7 +6,7 @@ export function addLocalsConfiguration() {
 	return (req, res, next) => {
 		res.locals.config = {
 			headerTitle: 'Manage casework',
-			styleFile: 'style-b7cc95f2.css'
+			styleFile: 'style-a17c5b26.css'
 		};
 		next();
 	};
